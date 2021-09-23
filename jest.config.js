7b83f42a6d881b0d1691d790110664dead5fd566
Julián LB <loperabarrientos@gmail.com>
2021-09-23 18:01:52 -0500

    Add roots reference to jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules"],
 };
